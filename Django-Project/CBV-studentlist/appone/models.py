1176cc1fe476a8a111245813d9e807b1d9ddad8f
Mani015 <ramanadhulamanikumar01975@gmail.com>
2022-12-06 15:02:20 +0530

    'py'

--- Django-Project/CBV-studentlist/appone/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    Firstname=models.CharField(max_length=30)
-    Lastname=models.CharField(max_length=30)
-    Marks=models.FloatField()
